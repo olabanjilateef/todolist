37b3644458f1ab82bb62c27ffdf693f8db7c5e9a
olabanjilateef <olabanjilateef1@gmail.com>
2023-12-14 12:29:01 +0100

    todoapp

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import TodoWrapper from './Component/TodoWrapper';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper  />
+    </div>
+  );
+}
+
+export default App;
